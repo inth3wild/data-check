a4fdaee1cea39a5512d41cc64ae33955aaec1134
inth3wild <toochukeuchukwu@gmail.com>
2020-12-03 23:15:19 +0100

    code refactoring

--- Login.py
+++ Login.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# read username and password from file
-# make req to /login endpoint
 import requests
 import argparse
 import re
@@ -10,124 +8,98 @@ from urllib.parse import unquote
 from concurrent.futures import ThreadPoolExecutor
 
 
-proxy = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
+class CheckData:
+    def __init__(self):
+        self.url = "https://netsurf.lmu.edu.ng/ajax/bals.php"
+        self.s = requests.Session()
+        self.s.get(self.url)
 
-url = "https://netsurf.lmu.edu.ng/ajax/bals.php"
-
-creds = {
-
-}
-
-parsed_data = {
-
-}
-
-
-tails = []
-
-s = requests.Session()
-s.get(url)
+        self.header = {"Accept-Encoding":"gzip", "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"}
+        self.credentials = {}
+        self.processed_response = {}
+        self.tails = []
+        # self.proxy = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
 
 
+    def add_flags(self):
+        ''' The function that gets all flags'''
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-f', type=str, help='The file that contains usernames and passwords')
+        args = parser.parse_args()
+        # The flag passed from the command line
+        file = args.f
+        # print(file)
+        return file
 
 
-header = {"Accept-Encoding":"gzip", "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"}
-
-
-
-def add_flags():
-    ''' The function that gets all flags'''
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', type=str, help='The file that contains usernames and passwords')
-
-    args = parser.parse_args()
-
-    # The flag passed from the command line
-    file = args.f
-    return file
-    # print(file)
-
-def get_creds():
-    '''get contents of creds.txt and append to tails list'''
-    file = add_flags()
-    #open, get contents of creds.txt and append to creds dictionary
-    fileObject = open(file, 'r')
-    fileContents = fileObject.read()
-    arr = fileContents.split("\n")
-
-    # remove all appearances of empty strings from the array/list
-    arr = [i for i in arr if i != ""]
-    
-    # print(arr)
-
-    for i in arr:
-        arr = i.split(":")
-        creds[arr[1]] = arr[0]
-        tails.append(arr[1])
-    # print(creds)
+    def get_creds(self):
+        '''get contents of credentials.txt and append to tails list'''
+        file = self.add_flags()
+        #open, get contents of credentials.txt and append to credentials dictionary
+        fileObject = open(file, 'r')
+        fileContents = fileObject.read()
+        arr = fileContents.split("\n")
+        # remove all appearances of empty strings from the array/list
+        arr = [i for i in arr if i != ""]  
+        # print(arr)
+        for i in arr:
+            arr = i.split(":")
+            self.credentials[arr[1]] = arr[0]
+            self.tails.append(arr[1])
+        # print(self.credentials)
         
 
+    def login(self, tail):
+        # r = requests.post(url1, data={"username":"chukwu.toochukwu", "password":"", "submit":"Submit"}, proxies=proxy, verify=False)
+        r = self.s.post(self.url, data=unquote(urlencode({"head":'["shgBKOVPjxE=","G3EkHHMA/aU=","olvO0CaThvf4kUsw","olvO0CaThvf4kUsw"]', "tail":f'{tail}'})), headers=self.header)
+        # print(r.status_code)
+        # print(r.text)
+        if "Invalid Username/Password" in r.text:
+            name = self.credentials[tail]
+            datadigits = ["Invalid Username/Password"]
+            # print("invalid")
+        else:
+            name = re.search("([a-z]*\.[a-z]*|[A-Za-z0-9]+[0-9]{4,5})", r.text)
+            datadigits = re.findall("-?[0-9]{1,3}\.[0-9]{1,3} [A-Za-z]*", r.text)
+            name = name.group(0)
 
-# r = requests.post(url1, data={"username":"chukwu.toochukwu", "password":"", "submit":"Submit"}, proxies=proxy, verify=False)
+        self.update_database(name, datadigits)
+        
 
-# e = urlencode(creds)
-# d = unquote(e)
+    def go_fast(self, tails):
+        ''''''
+        with ThreadPoolExecutor() as executor:
+            return executor.map(self.login, self.tails)
 
-# r = requests.post(url, data=d, headers=header, cookies=cookie, proxies=proxy, verify=False)
 
-def login(tail):
-    r = s.post(url, data=unquote(urlencode({"head":'["shgBKOVPjxE=","G3EkHHMA/aU=","olvO0CaThvf4kUsw","olvO0CaThvf4kUsw"]', "tail":f'{tail}'})), headers=header)
-    # print(r.status_code)
-    # print(r.text)
-    if "Invalid Username/Password" in r.text:
-        name = creds[tail]
-        datadigits = ["Invalid Username/Password"]
-        # print("invalid")
-    else:
-        name = re.search("([a-z]*\.[a-z]*|[A-Za-z0-9]+[0-9]{4,5})", r.text)
-        datadigits = re.findall("-?[0-9]{1,3}\.[0-9]{1,3} [A-Za-z]*", r.text)
-        name = name.group(0)
-
-    update_database(name, datadigits)
-
-    # return name, datadigits
-    # print(name, datadigits)
-    # regex_on_result(r)    
-
+    def update_database(self, name, datadigits):
+        self.processed_response[name] = datadigits
+        # print(name, datadigits)
     
 
+    def display_positive_results(self, processed_response):
+        print("Username", "\t", "Total Bandwidth", "\t", "Total Used", "\t", "Total Balance")
+        print("----------------------------------------------------------------------------")
+        for i in processed_response:
+            # print( len(processed_response[i]) )
+            if len(processed_response[i]) > 1:
+                print(i, "-->", "\t", processed_response[i][0], "\t", processed_response[i][1], "\t", processed_response[i][2])
 
 
-def go_fast(tails):
-    ''''''
-    with ThreadPoolExecutor() as executor:
-        return executor.map(login, tails)
-
-def update_database(name, datadigits):
-    parsed_data[name] = datadigits
-    # print(name, datadigits)
-    
-
-def display_positive_results(parsed_data):
-    print("Username", "\t", "Total Bandwidth", "\t", "Total Used", "\t", "Total Balance")
-    print("----------------------------------------------------------------------------")
-    for i in parsed_data:
-        # print( len(parsed_data[i]) )
-        if len(parsed_data[i]) > 1:
-            print(i, "\t", parsed_data[i][0], "\t", parsed_data[i][1], "\t", parsed_data[i][2])
-
-def display_negative_results(parsed_data):
-    print("\n")
-    for i in parsed_data:
-        if len(parsed_data[i]) == 1:
-            print(i, "\t", parsed_data[i][0])
+    def display_negative_results(self, processed_response):
+        print("\n")
+        for i in processed_response:
+            if len(processed_response[i]) == 1:
+                print(i, "-->", "\t", processed_response[i][0])
 
 
+    def start(self):
+        self.get_creds()
+        self.go_fast(self.tails)
 
-get_creds()
+        self.display_positive_results(self.processed_response)
+        self.display_negative_results(self.processed_response)
 
-go_fast(tails)
 
-display_positive_results(parsed_data)
-display_negative_results(parsed_data)
-
+bot = CheckData()
+bot.start()
